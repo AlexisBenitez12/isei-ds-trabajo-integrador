23db7c48a4fd29c4ef4ed255455d92979ea2b42e
Alexis <alexisfacundobenitez@gmail.com>
2020-10-27 22:37:08 -0300

    trabajo terminado

--- api/CuentaCorrientes.php
+++ api/CuentaCorrientes.php
@@ -6,36 +6,44 @@ header('Content-Type: application/json');
 require_once 'modelo/CuentaCorriente.php';
 require_once 'modelo/Proveedor.php';
 
+$provee = new Proveedor();
+$provee->Cuit= "20-1232444-3";
+$provee->Email= "adidas@gmail.com";
+$provee->RazonSocial= "Adidas";
+
+$provee2 = new Proveedor();
+$provee2->Cuit= "20-12554444-3";
+$provee2->Email= "umbro@gmail.com";
+$provee2->RazonSocial= "umbro";
+
+$provee3 = new Proveedor();
+$provee3->Cuit= "20-1232000-3";
+$provee3->Email= "nike@gmail.com";
+$provee3->RazonSocial= "nike";
+
+$provee4 = new Proveedor();
+$provee4->Cuit= "20-1232232-3";
+$provee4->Email= "puma@gmail.com";
+$provee4->RazonSocial= "puma";
+
 $cc1 = new CuentaCorriente();
 $cc1->NroCuentaCorriente= 20;
-$cc1->RazonSocial= "Adidas";
-$cc1->Proveedor= "1";
-$cc1->Cuit= "20-1232444-3";
-$cc1->Email= "adidas@gmail.com";
 $cc1->Saldo= "$10.000";
+$cc1->Proveedor= $provee;
 
 $cc2 = new CuentaCorriente();
 $cc2->NroCuentaCorriente= 12;
-$cc2->RazonSocial= "umbro";
-$cc2->Proveedor= "2";
-$cc2->Cuit= "20-12554444-3";
-$cc2->Email= "umbro@gmail.com";
 $cc2->Saldo= "$10.000";
+$cc2->Proveedor= $provee2;
 
 $cc3 = new CuentaCorriente();
 $cc3->NroCuentaCorriente= 15;
-$cc3->RazonSocial= "nike";
-$cc3->Proveedor= "3";
-$cc3->Cuit= "20-1232000-3";
-$cc3->Email= "nike@gmail.com";
 $cc3->Saldo= "$16.400";
+$cc3->Proveedor= $provee3;
 
 $cc4 = new CuentaCorriente();
 $cc4->NroCuentaCorriente= 10;
-$cc4->RazonSocial= "puma";
-$cc4->Proveedor= "4";
-$cc4->Cuit= "20-1232232-3";
-$cc4->Email= "puma@gmail.com";
+$cc4->Proveedor= $provee4;
 $cc4->Saldo= "$14.000";
 
 $array = array();
